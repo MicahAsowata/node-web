b0a6053042d203fe51f1f6830c166c91b4d6dc90
Micah Asowata <andrewgary963@gmail.com>
2023-05-04 07:26:50 +0100

    Complete server setup with routing and static file serving using raw Node

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const http = require("http");
 const fs = require("fs");
-
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-
-  fs.readFile("./views/index.html", (err, data) => {
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err.message);
       res.end();
     } else {
-      //   res.write(data);
-      res.end(data);
+      res.write(data);
+      res.end();
     }
   });
 });
